f3e513ebfd1c3a030af4feca1639ff7e16805772
lharo <esteban6000@gmail.com>
2020-11-07 22:27:09 +0000

    Added spring repository

---
+++ spring/jormundgander/src/main/java/com/lharo/jormundgander/JormundganderApplication.java
@@ -0,0 +1,13 @@
+package com.lharo.jormundgander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JormundganderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JormundganderApplication.class, args);
+	}
+
+}
---
+++ spring/jormundgander/src/test/java/com/lharo/jormundgander/JormundganderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lharo.jormundgander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JormundganderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
